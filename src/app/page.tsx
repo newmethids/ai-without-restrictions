68154a5c7d1f3dd42bf281ab4d943ec30a82857a
garrettow <garrettowgithub@gmail.com>
2025-09-26 19:40:58 -0400

    update build issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Sites } from "./sites";
 
 export default function Home() {
--- src/app/sites.tsx
+++ src/app/sites.tsx
@@ -9,7 +9,7 @@ export const Sites = () => {
         <ol className={"flex gap-2 flex-col"}>
             {sitelist.map(x => {
                 return (
-                    <li>
+                    <li key={x.name}>
                         <a target={"_blank"} href={x.link}>{x.name}</a>
                     </li>
                 )
