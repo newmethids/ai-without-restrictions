cd5b6905125016bb6ae11a912390e37808aac137
garrettow <garrettowgithub@gmail.com>
2025-09-26 19:26:57 -0400

    added tagline. update styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="flex flex-col items-center justify-center gap-4 p-4">
       <h1 className={"font-black text-6xl"}>AI Without Restrictions</h1>
+      <h2>Open source directory of unrestricted ai tools. Github <a className={"underline"} target={"_blank"} href={"https://github.com/newmethids/ai-without-restrictions"}>here</a></h2>
       <ol>
         <li>Spicygen</li>
         <li>Promptchan</li>
@@ -12,6 +13,8 @@ export default function Home() {
         <li>Sugarlab</li>
         <li>Lovescape</li>
         <li>DreamCompanion</li>
+        <li>GirlfriendGPT</li>
+        <li>CandyAI</li>
       </ol>
     </div>
   );
