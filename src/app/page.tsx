32f273e1f2da850ff3ff8b72a58b3d7b6d5ec00c
garrettow <garrettowgithub@gmail.com>
2025-09-26 21:51:23 -0400

    update tagline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-4 p-4">
       <h1 className={"font-black text-6xl text-center"}>AI Without Restrictions</h1>
-      <h2 className={"text-center"}>Open source directory of unrestricted ai tools. Github <a className={"underline"} target={"_blank"} href={"https://github.com/newmethids/ai-without-restrictions"}>here</a></h2>
+      <h2 className={"text-center"}>i review unrestricted nsfw unfiltered ai sites (the legal ones) . Code is open sourced on Github <a className={"underline"} target={"_blank"} href={"https://github.com/newmethids/ai-without-restrictions"}>here</a></h2>
       <Sites />
     </div>
   );
