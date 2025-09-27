6f0f21da5071a5accf85701ec7203fc90fb98f29
garrettow <garrettowgithub@gmail.com>
2025-09-26 21:19:41 -0400

    add link icon

--- src/app/sites.tsx
+++ src/app/sites.tsx
@@ -10,7 +10,7 @@ export const Sites = () => {
             {sitelist.map(x => {
                 return (
                     <li key={x.name}>
-                        <a target={"_blank"} href={x.link}>{x.name}</a>
+                        <a className={"flex gap-2 items-center justify-center"} target={"_blank"} href={x.link}>{x.name}<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-external-link-icon lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg></a>
                     </li>
                 )
             })}
