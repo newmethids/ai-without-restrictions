62eb491ac50dbd0a25b1638ce572c8d7d95298ae
garrettow <garrettowgithub@gmail.com>
2025-09-26 21:47:27 -0400

    add some descriptions

--- src/app/sitelist.ts
+++ src/app/sitelist.ts
@@ -1,35 +1,43 @@
 type site = {
     name: string,
     link: string,
+    description: string,
 }
 
 export const sitelist: site[] = [
     {
         name: "Spicygen",
-        link: "https://spicygen.ai/?ref=mme3ytq"
+        link: "https://spicygen.ai/?ref=mme3ytq",
+        description: "nsfw ai porn generating app. mostly focused on it's ai image to video generating feature. , but has general nsfw image generating feature and nsfw video generating capabilites."
     },
     {
         name: "Promptchan",
-        link: "https://promptchan.com/m/G1ubXL34SvaTaXMrev0TToWZFsV2/trend?landing=/ai-porn"
+        link: "https://promptchan.com/m/G1ubXL34SvaTaXMrev0TToWZFsV2/trend?landing=/ai-porn",
+        description: "ai porn video and image generating site. lots of customization. very good quality. "
     },
     {
         name: "AiAllure",
-        link: "https://www.aiallure.com/generate-ai-porn-nsfw/videos?ref=2xhQYT48U1BrBFQfgFc7Bohoik1"
+        link: "https://www.aiallure.com/generate-ai-porn-nsfw/videos?ref=2xhQYT48U1BrBFQfgFc7Bohoik1",
+        description: "if youre lonely and need a girlfriend you can create one with this app . and when youre horny you can create some porn too. "
     },
     {
         name: "Ourdream",
-        link: "https://ourdream.ai/generate?ref=nsfwgen"
+        link: "https://ourdream.ai/generate?ref=nsfwgen",
+        description: "ai girlfriend app. from first impression you can tell the devs put some effort into the site. the ai girls look good, the site looks good ."
     },
     {
         name: "GirlfriendGPT",
-        link: "https://t.mbsrv2.com/367665/7477?popUnder=true&aff_sub5=SF_006OG000004lmDN"
+        link: "https://t.mbsrv2.com/367665/7477?popUnder=true&aff_sub5=SF_006OG000004lmDN",
+        description: "girlfriendgpt? chatgpt for girlfriends I guess "
     },
     {
         name: "CandyAI",
-        link: "https://t.mbsrv2.com/367665/6646?popUnder=true&aff_sub5=SF_006OG000004lmDN"
+        link: "https://t.mbsrv2.com/367665/6646?popUnder=true&aff_sub5=SF_006OG000004lmDN",
+        description: "the og ai girlfriend app. "
     },
     {
         name: "Lovescape",
-        link: "https://t.mbsrv2.com/367665/7887?popUnder=true&aff_sub5=SF_006OG000004lmDN"
+        link: "https://t.mbsrv2.com/367665/7887?popUnder=true&aff_sub5=SF_006OG000004lmDN",
+        description: ""
     }
 ]
